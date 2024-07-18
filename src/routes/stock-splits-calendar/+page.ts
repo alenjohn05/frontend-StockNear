db582877d37b95c1ae05b6ccb6f01e4a752061c2
Alen John <alen@vizru.com>
2024-07-18 19:16:47 +0530

    updated the stock split

--- src/routes/stock-splits-calendar/+page.ts
+++ src/routes/stock-splits-calendar/+page.ts
@@ -4,7 +4,7 @@ import { userRegion, getCache, setCache } from '$lib/store';
 const usRegion = ['cle1','iad1','pdx1','sfo1'];
 
 let apiURL;
-
+let backendURL = import.meta.env.VITE_BACKEND_API_URL;
 userRegion.subscribe(value => {
 
   if (usRegion.includes(value)) {
@@ -43,9 +43,36 @@ export const load = async () => {
 
     return output;
   };
+  const getStockSplitsCalendarMain = async () => {
+    let output;
+
+    // Get cached data for the specific tickerID
+    const cachedData = getCache('', 'getStockSplitsCalendarMain');
+    if (cachedData) {
+      output = cachedData;
+    } else {
+      
+
+      // make the POST request to the endpoint
+      const response = await fetch(backendURL + '/get_daily_corporate_actions?EventType=12', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      output = await response.json();
+
+      // Cache the data for this specific tickerID with a specific name 'getStockSplitsCalendarMain'
+      setCache('', output, 'getStockSplitsCalendarMain');
+    }
+
+    return output;
+  };
 
   // Make sure to return a promise
   return {
-    getStockSplitsCalendar: await getStockSplitsCalendar()
+    getStockSplitsCalendar: await getStockSplitsCalendar(),
+    getStockSplitsCalendarMain: await getStockSplitsCalendarMain()
   };
 };
